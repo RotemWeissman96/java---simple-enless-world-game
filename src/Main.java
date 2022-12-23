551112bddb82acc7661ec628f92cca44dd2e6703
aviku <avi.kupinsky@mail.huji.ac.il>
2022-12-23 11:59:34 +0200

    second test avi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("avi");
+        System.out.println("avi kupinsky");
     }
 
     public void rotemTesting(){
