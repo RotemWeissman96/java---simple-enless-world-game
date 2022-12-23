38c6ea369368d9a0206db6d402394587d4805e04
rotem <rotem.weissman@mail.huji.ac.il>
2022-12-23 11:58:15 +0200

    test rotem3

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,6 @@ public class Main {
     public void rotemTesting(){
         System.out.println("Hello world!");
         System.out.println("rotem");
+        System.out.println("rotem");
     }
 }
